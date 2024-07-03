07c5ac27be381960366461590499529ede490a25
Prash6060 <litprashiljain922@gmail.com>
2024-07-03 20:13:14 +0530

    json issue resolved

--- models/user-login-model.js
+++ models/user-login-model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const jwt = require('jsonwebtoken'); // Add this 
 // Define the User schema
 const userSchema = new Schema({
   email: {
